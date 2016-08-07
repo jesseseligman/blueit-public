9e7bd7e5d7914a722597aa06915fbba1441be8fd
Jesse Seligman <jesseseligman@gmail.com>
2016-08-07 16:57:40 -0700

    combine posts and topics controllers

--- public/js/controllers/posts.js
+++ public/js/controllers/posts.js
@@ -5,9 +5,24 @@
 
   app.controller('postsCtrl', postsCtrl);
 
-  postsCtrl.$inject = ['postsSvc'];
+  postsCtrl.$inject = ['postsSvc', 'topicsSvc'];
 
-  function postsCtrl(postsSvc) {
+  function postsCtrl(postsSvc, topicsSvc) {
+    // Variables for topics
+    this.topics = [];
+    this.selectedTopic = 'All';
+    this.postTopic = 'Create New';
+    this.newTopic = '';
+
+    this.formatField = () => {
+      if (this.postTopic !== 'Create New') {
+        this.newTopic = '';
+      }
+    };
+
+    this.isSelected = (id) => id === Number.parseInt(this.selectedTopic) || this.selectedTopic === 'All';
+
+    // Variables for posts
     this.sortBy = '-rating';
     this.newPost = {};
     this.posts = [];
@@ -20,35 +35,43 @@
       post.rating -= 1;
     };
 
-    this.submitPost = function(newPost, postTopic, newTopic) {
-      if (newPost.$invalid) {
+    this.submitPost = function() {
+      if (this.postForm.$invalid) {
         return console.log('invalid');
       }
-      if (postTopic === 'Create New') {
-        postsSvc.submitTopic(newTopic)
+      if (this.postTopic === 'Create New') {
+        postsSvc.submitTopic(this.newTopic)
           .then((topicId) => {
             return postsSvc.submitPost(topicId, this.newPost)
           })
           .then((res) => {
-            this.newPost = {};
-            this.postForm.$setPristine();
-            $('#post-modal').closeModal();
-            activate();
+            this.submitActions();
           })
           .catch((err) => {
             throw err;
           });
       }
       else {
-        return postsSvc.submitPost(postTopic, this.newPost)
+        return postsSvc.submitPost(this.postTopic, this.newPost)
           .then((res) => {
-            this.newPost = {};
-            activate();
+            this.submitActions();
           })
           .catch((err) => {
             throw err;
           });
       }
+    };
+
+    // Function to be called after form submissions to reset variables
+    this.submitActions = () => {
+      this.newPost = {};
+      this.newTopic = '';
+      this.postTopic = 'Create New';
+      this.selectedTopic = 'All';
+      this.postForm.$setPristine();
+      $('#post-modal').closeModal();
+      this.formatField();
+      activate();
     };
 
     const activate = () => {
@@ -58,7 +81,14 @@
         })
         .catch((err) => {
           throw err;
+        });
+      topicsSvc.getTopics()
+        .then((topics) => {
+          this.topics = topics;
         })
+        .catch((err) => {
+          throw err;
+        });
     };
 
     activate();
